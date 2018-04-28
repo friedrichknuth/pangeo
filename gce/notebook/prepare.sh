f88702985f694aae0eedb8ce79124b96a47673df
Friedrich Knuth <knuth@marine.rutgers.edu>
2018-04-28 16:01:24 -0400

    Update configureation

--- gce/notebook/prepare.sh
+++ gce/notebook/prepare.sh
@@ -2,8 +2,11 @@
 
 set -x
 
-echo "Copy files from pre-load directory into home"
-cp --update -r -v /pre-home/. /home/jovyan
+echo "Cloning repo with notebook examples"
+git clone "$PROJECT_NOTEBOOKS"
+
+# echo "Copy files from pre-load directory into home"
+# cp --update -r -v /pre-home/. /home/jovyan
 
 if [ -e "/opt/app/environment.yml" ]; then
     echo "environment.yml found. Installing packages"
